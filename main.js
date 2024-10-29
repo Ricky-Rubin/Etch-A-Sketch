be06be656ebd47da4ab1b57c9ce3861c889242eb
Ricky-Rubin <owolabi1738@gmail.com>
2024-10-29 22:36:24 +0100

    Modified HTML file to include buttons for color
    Each button will change the default color that the divs have as a
    background when the mouse hovers over them to the selected color.

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
+let paint = 'black';
+
 const container = document.querySelector('#container');
 const button = document.querySelector('#btn');
+const clearBtn = document.querySelector('#clear');
+const blackBtn = document.querySelector('#default');
+const redBtn = document.querySelector('#red');
+const greenBtn = document.querySelector('#green');
 
 function populateDiv() {
-    let question = prompt('Enter your preferred grid size? Input only one of the numbers.');
+    let question = prompt('Enter the number of squares you want per side.');
 
-    let length = 500;
+    let length = 350;
     let divNo = question * question;
     let spaces = 2 * question;
     let newLength = length - spaces;
@@ -12,13 +18,37 @@ function populateDiv() {
 
     for (let divs = 0; divs < divNo; divs++) {
         const square = document.createElement('div');
-        square.style.cssText = `width: ${divLength}px; height: ${divLength}px; border: 1px solid black;`;
+        square.style.cssText = `width: ${divLength}px; height: ${divLength}px; border: 1px solid lightgrey; background-color: white;`;
         container.appendChild(square);
 
         square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = 'black';
+            square.style.backgroundColor = `${paint}`;
         })
+    }
+
+    if (question < 2 || question > 100) {
+        alert('Enter a value greater than 2 and not more than 100!');
+        container.innerHTML = '';
     }
 }
 
-button.addEventListener('click', populateDiv);
+button.addEventListener('click', () => {
+    container.innerHTML = '';
+    populateDiv();
+});
+
+clearBtn.addEventListener('click', () => {
+    container.innerHTML = '';
+})
+
+blackBtn.addEventListener('click', () => {
+    paint = 'black';
+});
+
+redBtn.addEventListener('click', () => {
+    paint = 'red';
+});
+
+greenBtn.addEventListener('click', () => {
+    paint = 'green';
+});
